4b1a8e1f1f2e989393dd55c719948da1b9bf6bca
liyongxiang <liyongxiang@hoolai.com>
2016-11-07 18:37:09 +0800

    Initial commit

---
+++ app/src/main/java/com/lyx/imitation/xmlyfm/activity/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.lyx.imitation.xmlyfm.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/11/7.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
