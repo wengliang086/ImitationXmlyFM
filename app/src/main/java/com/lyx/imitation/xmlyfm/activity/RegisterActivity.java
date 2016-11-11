0128ffc6b3a96438dc0e45afbd85c06d8dae74ee
liyongxiang <liyongxiang@hoolai.com>
2016-11-11 16:10:55 +0800

    页面布局基本完成

---
+++ app/src/main/java/com/lyx/imitation/xmlyfm/activity/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.lyx.imitation.xmlyfm.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lyx.imitation.xmlyfm.R;
+
+/**
+ * Created by Administrator on 2016/11/11.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
--- app/src/main/java/com/lyx/imitation/xmlyfm/fragment/MineFragment.java
+++ app/src/main/java/com/lyx/imitation/xmlyfm/fragment/MineFragment.java
@@ -1,5 +1,6 @@
 package com.lyx.imitation.xmlyfm.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.lyx.imitation.xmlyfm.R;
+import com.lyx.imitation.xmlyfm.activity.RegisterActivity;
 
 /**
  * Created by Administrator on 2016/11/8.
@@ -25,6 +27,18 @@ public class MineFragment extends BaseFragment {
     }
 
     private void initView() {
-
+        baseView.findViewById(R.id.id_mine_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().startActivity(new Intent(getActivity(), RegisterActivity.class));
+                getActivity().overridePendingTransition(R.anim.activity_in, R.anim.activity_out);
+                // 实现淡入浅出的效果
+//                getActivity().overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                // 由左向右滑入的效果
+//                getActivity().overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                // 实现zoommin 和 zoomout (自定义的动画)
+//                getActivity().overridePendingTransition(R.anim.zoomin, R.anim.zoomout);
+            }
+        });
     }
 }
